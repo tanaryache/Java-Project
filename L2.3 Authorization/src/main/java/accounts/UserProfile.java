ba5fa8dc9f7fb307c3cfef46fcdcc6ba7bc1973f
tanaryache <116083643+tanaryache@users.noreply.github.com>
2024-05-05 17:30:33 +0500

    Add files via upload

---
+++ L2.3 Authorization/src/main/java/accounts/UserProfile.java
@@ -0,0 +1,32 @@
+package accounts;
+
+
+public class UserProfile {
+    private final String login;
+    private final String password;
+    private final String email;
+
+    public UserProfile(String login, String password, String email) {
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+    public UserProfile(String login) {
+        this.login = login;
+        this.password = login;
+        this.email = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPass() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
